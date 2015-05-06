1ea8083d09432e7109e2f022bdf2ec10ca63a4c8
Quentin BERLEMONT <quentinberlemont@gmail.com>
2015-05-06 21:59:02 +0200

    modficications du server.js pour y inclure les dev de primus sur le tchat

--- server.js
+++ server.js
@@ -13,13 +13,24 @@ var app = IoC.create('app/application');
 
 
 var server = IoC.create('http').createServer(app);
-var Primus = IoC.create('primus');
-var primus = new Primus(server, {transformer: 'websockets'});
 
+var Primus = IoC.create('primus');
+var primusMultiplex = IoC.create('primus-multiplex');
+var primusEmitter = IoC.create('primus-emitter');
+var primusResource = IoC.create('primus-resource');
+
+var primus = new Primus(server, {transformer: 'websockets'});
+primus.use('multiplex', primusMultiplex);
+primus.use('emitter', primusEmitter);
+primus.use('resource', primusResource);
 
 primus.on('connection', function connection(spark) {
-    console.log('new connection');
-    spark.write({ Welcome: 'Hello!' });
+    // Si un utilisateur envoie un message
+    spark.on('tchat-send-message', function(data) {
+      // alors, notifie tout les utilisateurs
+      primus.send('tchat-send-message', data);
+      console.log(data);
+    });
 });
 
 primus.save(config.primusClientPath);
