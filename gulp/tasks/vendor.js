fef865d7a941632480650c89d397168244370bce
Quentin BERLEMONT <quentinberlemont@gmail.com>
2015-06-03 22:00:05 +0200

    modif tache vendor

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -12,10 +12,7 @@
     return plug.merge(
       gulp
         .src(bowerFiles.ext('css').files)
-        //.pipe(plug.newer(config.outputDir + 'client/assets/styles/vendor.min.css'))
-        .pipe(plug.concat('vendor.css'))
-        .pipe(gulp.dest(config.outputDir + config.client.styleDir))
-        .pipe(plug.rename({suffix: '.min'}))
+        .pipe(plug.concat('vendor.min.css'))
         .pipe(plug.bytediff.start())
         .pipe(plug.minifyCss())
         .pipe(plug.bytediff.stop(utils.bytediffFormatter))
@@ -27,10 +24,7 @@
         ),
       gulp
         .src(bowerFiles.ext('js').files)
-        //.pipe(plug.newer(config.outputDir + 'client/assets/scripts/vendor.min.js'))
-        .pipe(plug.concat('vendor.js'))
-        .pipe(gulp.dest(config.outputDir + config.client.scriptDir))
-        .pipe(plug.rename({suffix: '.min'}))
+        .pipe(plug.concat('vendor.min.js'))
         .pipe(plug.bytediff.start())
         .pipe(plug.uglify())
         .pipe(plug.bytediff.stop(utils.bytediffFormatter))
