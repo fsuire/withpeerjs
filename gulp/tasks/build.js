020eb2fe07bf8d46a7f15709c363b7d8049e256a
Frédéric Suire <fred_suire@yahoo.fr>
2015-06-09 21:32:16 +0200

    enore des corrections de gulp, et création de la tache gulp default

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,14 +3,13 @@
 
   // Build the application
 
-    var runSequence = require('run-sequence');
+  var runSequence = require('run-sequence');
 
   exports.task = function(callback) {
 
     if (IS_DEV) {
       runSequence(
         ['lint-js'],
-        ['clean'],
         ['stylus'],
         ['copy-client', 'copy-server'],
         ['inject'],
@@ -18,7 +17,6 @@
       );
     } else {
       runSequence(
-        ['clean'],
         ['lint-js'],
         ['stylus'],
         ['copy-server', 'vendor', 'application-minify'],
